4889ba6bd3fd07cecd414311af1497f87c2bab72
Mehdi Benrefad <benrefadmehdi@gmail.com>
2021-05-03 16:26:42 +0000

    Initial Commit

---
+++ IMC_Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IMC_Calculator
+//
+//  Created by Mehdi Benrefad on 03/05/2021.
+//  Copyright © 2021 Mehdi Benrefad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
